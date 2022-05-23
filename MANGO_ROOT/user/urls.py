f97d5fbef6252ea804945f38037979033a129ec8
CDGDG <whehdrjs207@naver.com>
2022-05-23 17:00:20 +0900

    base.html
    디자인, 파일 등등

--- MANGO_ROOT/user/urls.py
+++ MANGO_ROOT/user/urls.py
@@ -14,6 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('base/', views.base),
+    path('index/', views.index),
+    path('elements/', views.elements),
+    path('generic/', views.generic),
 ]
--- MANGO_ROOT/user/views.py
+++ MANGO_ROOT/user/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def base(request):
+    return render(request, 'base.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def elements(request):
+    return render(request, 'elements.html')
+
+def generic(request):
+    return render(request, 'generic.html')
