1461fa757d1b1a13f5330c952dcc5c807f07da7f
CDGDG <whehdrjs207@naver.com>
2022-05-25 15:12:33 +0900

    뜨라이씨끌 뚜 썸쁠래이쓰 바닐라 라뜨레 에뜨뜨레쓰 예욥

--- MANGO_ROOT/music/urls.py
+++ MANGO_ROOT/music/urls.py
@@ -13,13 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from music.views import home
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', home),
-    path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
-    path('music/', include('music.urls')), 
 ]
